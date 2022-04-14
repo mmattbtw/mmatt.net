2c156ece3871dc0cc842fd32cb35ab48e436694e
matt <30363562+mmattbtw@users.noreply.github.com>
2022-04-14 19:40:22 +0000

    awesome

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,10 +1,23 @@
 import { Container } from "@mantine/core";
+import { ArticleCardImage } from "components/BlogPreview";
+
+
+const blogPosts = [{
+  title: "test",
+  image: "https://http.cat/404",
+  category: "test"
+}]
 
 export default function BlogPage() {
   return (
     <>
       <Container>
         <h1>/blog</h1>
+        {
+          blogPosts.map(post => (
+            <ArticleCardImage {...post} />
+          ))
+        }
       </Container>
     </>
   );
