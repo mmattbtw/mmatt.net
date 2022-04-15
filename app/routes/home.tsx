b2d66657cdd53d588f5cc7b781dd62d5b08c3c9c
mmattbtw <30363562+mmattbtw@users.noreply.github.com>
2022-04-14 22:04:56 -0500

    redirect /home --> /

---
+++ app/routes/home.tsx
@@ -0,0 +1,11 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async () => {
+    return redirect("/");
+};
+
+export async function home() {
+    useLoaderData();
+}
