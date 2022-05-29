eb22ddaca6938e99cb7c704458b1fe99a24be092
mmattbtw <30363562+mmattbtw@users.noreply.github.com>
2022-05-28 21:42:02 -0500

    forsen

--- app/routes/testing/creatingcomments.tsx
+++ app/routes/testing/creatingcomments.tsx
@@ -1,15 +1,28 @@
-import { TextInput } from "@mantine/core";
-import { ActionFunction } from "@remix-run/node";
+import { Button, Container, TextInput } from "@mantine/core";
+import { ActionFunction, redirect } from "@remix-run/node";
 import { Form } from "@remix-run/react";
+import { authenticator } from "~/services/auth.server";
 import { createComment } from "~/services/comments.server";
+import { sessionType } from "~/types/typings";
 
 export const action: ActionFunction = async ({ request }) => {
     const formData = await request.formData();
+    const session: sessionType = await authenticator.isAuthenticated(request, {
+        failureRedirect: "/login",
+    })
+
+    if (!session) {
+        redirect("/login")
+    }
+
+    console.log(session.json.id)
   
     const content = formData.get("content") as string
 
     const project: any = {
-        content
+        content,
+        parentPostId: "1",
+        userId: session.json.id
     }
 
     const comment = await createComment(project)
@@ -19,7 +32,7 @@ export const action: ActionFunction = async ({ request }) => {
 
 export default function createCommentTestPage() {
   return (
-    <>
+    <Container>
         <Form method="post">
             <p>
                 <label>
@@ -30,7 +43,14 @@ export default function createCommentTestPage() {
                 />
                 </label>
             </p>
+            <p className="text-right">
+                <Button
+                type="submit"
+                >
+                Create Post
+                </Button>
+            </p>
         </Form>
-    </>
+    </Container>
   );
 }
