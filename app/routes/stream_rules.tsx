9050cc7490ab4c3a3df504b1d8596f3c290e87bf
mmattbtw <matt@mmatt.net>
2022-08-07 18:44:28 -0500

    add stream rules page

---
+++ app/routes/stream_rules.tsx
@@ -0,0 +1,17 @@
+import { Container } from '@mantine/core';
+
+export default function StreamRulesPage() {
+    return (
+        <Container>
+            <h1>/stream_rules</h1>
+
+            <ol>
+                <li>don't get banned</li>
+                <li>follow twitch TOS (or whatever platform)</li>
+                <li>
+                    read this <a href="https://xkcd.com/1357/">https://xkcd.com/1357/</a>
+                </li>
+            </ol>
+        </Container>
+    );
+}
