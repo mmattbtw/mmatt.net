156145b813cc11b15719c49af9cd64f4d57da05f
mmattbtw <matt@mmatt.net>
2022-08-18 23:47:17 -0500

    fix breakpoints later

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,6 +27,10 @@ module.exports = {
             opacity: 1,
           },
         },
+
+        colors: {
+          gradient: "linear-gradient(to right, #f12711, #f5af19)",
+        },
       },
     },
     plugins: [require("tailwindcss-animation-delay")],
