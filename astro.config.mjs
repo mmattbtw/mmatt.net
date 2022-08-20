c68d85b5d0bc344962b2b6840d9d08de9551f7e0
mmattbtw <matt@mmatt.net>
2022-08-20 00:55:28 -0500

    xdd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
+import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import prefetch from "@astrojs/prefetch";
-import image from "@astrojs/image";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), prefetch({
-    throttle: 10
-  }), image(), sitemap()]
-  experimental: {
-      integrations: true
-    }
+  integrations: [
+    tailwind(),
+    react(),
+    prefetch({
+      throttle: 10,
+    }),
+    image(),
+    sitemap(),
+  ],
 });
