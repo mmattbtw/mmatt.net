e86dd5503c5da15292980769273379e897ff223d
mmattbtw <matt@mmatt.net>
2022-08-20 00:56:28 -0500

    xdd

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mmatt.net",
   integrations: [
     tailwind(),
     react(),
