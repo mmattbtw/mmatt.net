50615a00c6b7ae610948ae0bc8d1c2a1d93e7a2d
mmattbtw <matt@mmatt.net>
2022-09-04 17:51:08 -0500

    xeddd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
-import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mmatt.net",
-  integrations: [tailwind(), prefetch({
-    throttle: 10
-  }), image(), sitemap(), preact()]
+  integrations: [
+    tailwind(),
+    prefetch({
+      throttle: 10,
+    }),
+    image(),
+    sitemap(),
+    preact(),
+  ],
 });
