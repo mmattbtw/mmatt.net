a7a631571ec1a52151223cf4f71188ab5de29ade
mmattbtw <matt@mmatt.net>
2022-09-18 13:46:22 -0500

    seo yuipee

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import createRobotsTxtIntegration from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  site: "https://mmatt.net",
+  experimental: {
+    integrations: true,
+  },
+  integrations: [preact(), tailwind(), sitemap(), createRobotsTxtIntegration()],
 });
