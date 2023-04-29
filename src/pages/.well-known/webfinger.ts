6813aa7693bf3cf2b1ee5c70239eff1e61e30e6a
mmattbtw <matt@mmatt.net>
2023-04-28 19:14:42 -0500

    mastodon username

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -3,18 +3,18 @@ import type { APIRoute } from "astro";
 export const get: APIRoute = async function get() {
   return {
     body: JSON.stringify({
-      subject: "acct:mm@social.lol",
-      aliases: ["https://social.lol/@mm", "https://social.lol/users/mm"],
+      subject: "acct:matt@social.lol",
+      aliases: ["https://social.lol/@matt", "https://social.lol/users/matt"],
       links: [
         {
           rel: "http://webfinger.net/rel/profile-page",
           type: "text/html",
-          href: "https://social.lol/@mm",
+          href: "https://social.lol/@matt",
         },
         {
           rel: "self",
           type: "application/activity+json",
-          href: "https://social.lol/users/mm",
+          href: "https://social.lol/users/matt",
         },
         {
           rel: "http://ostatus.org/schema/1.0/subscribe",
