59a9cb1d1c4361beeed76e2d7b32ccc229c3a7ef
mmattbtw <matt@mmatt.net>
2024-11-06 14:57:05 -0600

    cloudflare time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import createRobotsTxtIntegration from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://mmatt.net",
   output: "hybrid",
-  adapter: vercel({
-    webAnalytics: true,
-    functionPerRoute: false,
-  }),
+  adapter: cloudflare(),
   integrations: [tailwind(), sitemap(), createRobotsTxtIntegration()],
 });
