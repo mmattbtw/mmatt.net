5179be93a20f7d0b1a26171f683a90c738063db6
mmattbtw <matt@mmatt.net>
2024-11-11 13:34:59 -0600

    another project & favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,17 @@ export default async function Home() {
                 @mmatt.net
               </a>
             </h2>
+            <div className="flex flex-row gap-2">
+              <a className="underline scroll-smooth" href="#projects">
+                projects
+              </a>{" "}
+              -{" "}
+              <a className="underline scroll-smooth" href="#posts">
+                posts
+              </a>
+            </div>
           </div>
+
           <Image
             src="/matt.jpg"
             alt="stan from south park in a low quality picture from an xbox gamerpic"
@@ -25,6 +35,7 @@ export default async function Home() {
             className="rounded-full w-24 h-24"
           />
         </div>
+
         <p>
           technologist trying to make{" "}
           <a className="underline" href="https://songish.app">
@@ -49,11 +60,13 @@ export default async function Home() {
             ✉️ matt at mmatt.net
           </a>
           . don&apos;t try to contact me on instant messaging platforms (e.g.
-          discord or bluesky) if I don&apos;t know you, cold emails are great!
+          discord or bluesky) if I don&apos;t know you. cold emails are great!
         </p>
         <hr className="mt-5 mb-5" />
 
-        <h2 className="font-bold text-2xl">projects</h2>
+        <h2 className="font-bold text-2xl" id="projects">
+          projects
+        </h2>
         <p>
           various projects i&apos;ve started or contributed to on the{" "}
           <span title="not like the .NET framework, i just meant the internet">
@@ -63,35 +76,75 @@ export default async function Home() {
 
         <br />
 
-        <Project
-          {...{
-            name: "Songish",
-            byline:
-              "🛠️ work in progress - ⚙️ next.js, typescript, t3 stack - 🧑‍💻 head technologist",
-            date: "📅 Feb 2023 - Present",
-            description: (
-              <>
-                <p>
-                  practically my life&apos;s work... in progress. its the best
-                  place for music. music discussion, music discoverability,
-                  music events, music purchasing, music listening. it is what I
-                  want to do for the rest of my life, for real. every time I get
-                  to &quot;work&quot; a Songish event, when it comes to
-                  organizing and promoting, it is some of the most fun I have.
-                  and also seeing my ideas go from visions to products that I
-                  have implemented is incredible.
-                </p>
-                <br />
-                <p>rant aside, I just want to make this work.</p>
-              </>
-            ),
-            link: "https://songish.app?via=mmatt.net",
-          }}
-        />
+        <div className="flex flex-col gap-4">
+          <Project
+            {...{
+              name: "Songish",
+              byline:
+                "🛠️ work in progress - ⚙️ next.js, typescript, t3 stack - 🧑‍💻 head technologist",
+              date: "📅 Feb 2023 - Present",
+              description: (
+                <>
+                  <p>
+                    practically my life&apos;s work... in progress. its the best
+                    place for music. music discussion, music discoverability,
+                    music events, music purchasing, music listening. it is what
+                    I want to do for the rest of my life, for real. every time I
+                    get to &quot;work&quot; a Songish event, when it comes to
+                    organizing and promoting, it is some of the most fun I have.
+                    and also seeing my ideas go from visions to products that I
+                    have implemented is incredible.
+                  </p>
+                  <br />
+                  <p>
+                    rant aside, this is all still{" "}
+                    <a
+                      href="https://song.link/i/1484224395"
+                      className="underline"
+                    >
+                      🎵 coming soon, coming soon
+                    </a>
+                  </p>
+                </>
+              ),
+              link: "https://songish.app?via=mmatt.net",
+            }}
+          />
 
+          <Project
+            {...{
+              name: "AI TTS Donations (Solrock)",
+              byline:
+                "🗃️ archived - ⚙️ next.js, typescript, python, open source - 🧑‍💻 maintainer",
+              date: "📅 Oct 2021 - Oct 2023",
+              description: (
+                <>
+                  <p>
+                    AI TTS Donations (Solrock) was the first free and open
+                    source AI TTS (text to speach) program for Twitch donations.
+                    This was made because other solutions were private or were
+                    very pricy. AI TTS Donations included sound effects, voice
+                    effects, and thousands of voices using the Uberduck and
+                    FakeYou APIs.
+                  </p>
+                  <br />
+                  <p>
+                    Project shutdown in October of 2023 due to rising costs with
+                    limited budget. Sold redirection rights of the subdomain
+                    &quot;solrock.mmattdonk.com&quot; to TTS.Monster, another
+                    Twitch TTS service.
+                  </p>
+                </>
+              ),
+              link: "https://github.com/mmattdonk/ai-tts-donations?via=mmatt.net",
+            }}
+          />
+        </div>
         <hr className="mt-5 mb-5" />
 
-        <h2 className="font-bold text-2xl">posts</h2>
+        <h2 className="font-bold text-2xl" id="posts">
+          posts
+        </h2>
         <p>
           these are the blog posts on my website hosted on the ATProtocol, an
           open protocol for building social apps.
@@ -110,7 +163,7 @@ export default async function Home() {
             📃 PDSls
           </a>{" "}
           under{" "}
-          <span className="font-mono bg-gray-900 p-1 rounded-md outline outline-[0.5px]">
+          <span className="font-mono bg-gray-100 dark:bg-gray-900 p-1 rounded-md outline outline-[0.5px]">
             net.mmatt.blog.post
           </span>
         </p>
