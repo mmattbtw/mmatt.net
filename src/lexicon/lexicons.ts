3cdcb3d496ed46b4241c74a43f3f40188da1038a
mmattbtw <matt@mmatt.net>
2024-11-11 00:10:43 -0600

    next.js time

--- src/lexicon/lexicons.ts
+++
@@ -1,9 +0,0 @@
-/**
- * GENERATED CODE - DO NOT MODIFY
- */
-import { LexiconDoc, Lexicons } from '@atproto/lexicon'
-
-export const schemaDict = {}
-export const schemas: LexiconDoc[] = Object.values(schemaDict) as LexiconDoc[]
-export const lexicons: Lexicons = new Lexicons(schemas)
-export const ids = {}
